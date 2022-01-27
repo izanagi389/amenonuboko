2f8eeec81370bc5a266baee5ba04cbd3780c26d8
izanagi389 <izanagiokami389@gmail.com>
2022-01-28 03:48:29 +0900

    Bert＋fastapi版初期バージョン

--- config.py
+++ config.py
@@ -5,12 +5,21 @@ import os
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-import os
-MICROCMS_API_KEY = os.getenv('MICROCMS_API_KEY')
-MICROCMS_URL = os.getenv('MICROCMS_URL')
-LIMIT = os.getenv('LIMIT')
+MICROCMS_API_KEY = os.environ.get('MICROCMS_API_KEY')
+MICROCMS_URL = os.environ.get('MICROCMS_URL')
+LIMIT = os.environ.get('LIMIT')
 
-BERT_MODEL_NAME = os.getenv('BERT_MODEL_NAME')
-BERT_FINETUNING_MODEL_NAME = os.getenv('BERT_FINETUNING_MODEL_NAME')
-CL_TOHOKU_MODEL_NAME = os.getenv('CL_TOHOKU_MODEL_NAME')
+BERT_MODEL_NAME = os.environ.get('BERT_MODEL_NAME')
+BERT_FINETUNING_MODEL_NAME = os.environ.get('BERT_FINETUNING_MODEL_NAME')
+CL_TOHOKU_MODEL_NAME = os.environ.get('CL_TOHOKU_MODEL_NAME')
 COLUMNS = ["id", "title"]
+
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+MYSQL_HOST = 'db'
+MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
+SITE_ROOT_URL = os.environ.get('SITE_ROOT_URL')
+ORIGINS = [
+    "http://localhost:3000",
+    SITE_ROOT_URL,
+]
--- main.py
+++ main.py
@@ -1,27 +1,67 @@
-from app import app
-from api.related_handler import related_doc_list
-from flask_cors import CORS
-from app.scheduler import scheduler
-from apscheduler.schedulers.background import BackgroundScheduler
-import atexit
-from app.lib.db.title_model import init_data, cerate_title_table
+
+import config
+
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import json
+
+from app import crud, scheduler
 
 
-app.register_blueprint(related_doc_list)
-CORS(app)
-
-scheduler()
-sched = BackgroundScheduler(daemon=True)
-sched.add_job(scheduler, 'interval', days=1)
-
-sched.start()
-atexit.register(lambda: sched.shutdown())
-
-app.config.from_object('app.lib.db.config.Config')
-
-cerate_title_table()
-init_data()
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="3.0.3",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def init(background_tasks: BackgroundTasks):
+    background_tasks.add_task(
+        scheduler.scheduler.start(), message="some notification")
+
+
+@app.get("/api/")
+def read_root():
+    return json.dumps(["こちらは関連のブログタイトル抽出APIです。"], indent=2, ensure_ascii=False)
+
+
+@app.get("/api/v1/")
+def read_root():
+    return json.dumps(["パスが間違っています！"], indent=2, ensure_ascii=False)
+
+
+@app.get("/api/v1/releted_title/")
+def read_root():
+    return json.dumps(["パスパラメーターにブログIDを指定してください！"], indent=2, ensure_ascii=False)
+
+
+@app.get("/api/v1/releted_title/{search_id}")
+def read_item(search_id: str):
+    print(search_id)
+    if (search_id == None):
+        return json.dumps(["パラメータがありません！"], indent=2, ensure_ascii=False)
+
+    return JSONResponse(crud.get_related_titles(search_id))
