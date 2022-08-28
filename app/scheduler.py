202f38a4d6261750fa57f7da9345b35062a45c39
izanagi389 <izanagiokami389@gmail.com>
2022-08-28 15:30:31 +0900

    トピック抽出処理を追加

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,11 +1,26 @@
-import app.touch_model as transformers_model_lib
-from app import crud
+import app.lib.contents.microcms as m
+from app.lib.topic import topic
+from app.lib.related_score import create_score
+
+import datetime
+import config
 
 def init():
-    print("get model data")
-    transformers_model_lib.initialize_cl_tohoku_load()
-    print("got model")
-    print("init database")
-    crud.init_data()
-    print("finish")
 
+    print("Scheduling Start! at {}".format(datetime.datetime.today()))
+
+    url = config.MICROCMS_URL + "?limit=" + config.LIMIT
+    api_key = config.MICROCMS_API_KEY
+    columns = ["id", "title", "blogContent"]
+
+    content_list = m.getData(url, api_key, columns)
+
+    # トピックコーパスの作成
+    print("Creating Topic Corpus")
+    topic().start(content_list)
+    print("Created Topic Corpus")
+
+    # 関連記事のスコア作成（v2）
+    print("Creating Related Score")
+    create_score().start(content_list)
+    print("Created Related Score")
